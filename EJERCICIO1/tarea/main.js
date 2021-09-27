a12c1c3d154654ca6681422887a844d067360617
unknown <arellanoedgar265@gmail.com>
2021-09-27 15:08:09 -0500

    calculadora

---
+++ EJERCICIO1/tarea/main.js
@@ -0,0 +1,18 @@
+var persona = {
+    "Nombres" : "edgar yair",
+    "Apellidos" : "Arellano Del rio",
+    "Edad" : 18,
+    "Estudios" : {
+        "Nombre universidad" : "UPT",
+        "Carrera" : "Ingeniería en Software",
+        "Cuatrimestre" : 4
+    },
+    "Gustos" : {
+        "Genero de música" : "rap, hip-hop",
+        "Canción favorita" : "ninguna",
+        "Deporte favorito" : "futbol",
+        "Epoca favorita del año" : "navidad"
+
+    }
+}
+console.log(persona);
---
+++ EJERCICIO1/trabajo en clase/main.js
@@ -0,0 +1,28 @@
+
+//funcion
+var palabraBuscar =  prompt("ingresa la palabra para jugar");
+
+    var palabraBuscar2 = prompt("ingresa la palabra a buscar");
+
+    if (palabraBuscar==palabraBuscar2){
+        console.log("su palabra es correcta");
+        console.log("su palabra es:" + palabraBuscar);
+    
+    }
+    else{
+        console.log("su palabra es incorrecta");
+        
+    }
+
+    
+
+
+
+
+
+
+
+
+
+//intentos
+
